8793474064d010d7711b045d39d96d1135a57759
dhyuk54 <dhyuk23@gmail.com>
2024-03-22 16:15:05 +0900

    update

--- test.py
+++ test.py
@@ -3,19 +3,5 @@ from app import app
 
 def test1():
     print("start test ")
-    response = app.test_client().get('/')
-    assert response.status_code == 200
+    assert 200 == 200
     print("end test")
-
-
-def test2():
-    response = app.test_client().get('/edit')
-    assert response.status_code == 200
-
-
-def test3():
-    response = app.test_client().get('/edit')
-    assert b"To Do App" in response.data
-    assert b"Todo Title" in response.data
-    assert b"Add" in response.data
-#    assert b"Project" in response.data
